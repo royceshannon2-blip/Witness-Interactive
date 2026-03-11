67e033978696bdfac2b5a8043dfa0a7fa21c59ae
royceshannon2-blip <royceshannon2@gmail.com>
2026-03-10 22:27:32 -0400

    chore(version): bump to 1.4.2

--- config/version.js
+++ config/version.js
@@ -2,5 +2,5 @@
  * version.js - Single source of truth for application version
  */
 
-export const VERSION = '1.4.1';
-export const VERSION_DATE = '2026-03-10';
+export const VERSION = '1.4.2';
+export const VERSION_DATE = '2026-03-11';
