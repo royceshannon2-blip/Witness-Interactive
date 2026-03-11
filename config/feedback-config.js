ec617f742f7aed49e0e6d0ad02a4deea9d46cefe
royceshannon2-blip <royceshannon2@gmail.com>
2026-03-11 15:28:43 -0400

    Update endpoint URL for Google Sheets integration

--- config/feedback-config.js
+++ config/feedback-config.js
@@ -12,7 +12,7 @@ export const FEEDBACK_CONFIG = {
   transportType: 'googleSheets',
 
   // Endpoint URL for Google Sheets Apps Script or email webhook
-  endpointUrl: 'https://script.google.com/macros/s/AKfycbxjn-L3-qQNwBCCF_2BWwkEexJ-SS_KXjvE2LdZ9PRlymPScVQKZy36z_i9AK5ezb33/exec',
+  endpointUrl: 'https://script.google.com/macros/s/AKfycbweba5jTkImqtO7aAEnEuERO-00LlRtJ7cpxPiYcd_ObKiFoW0BD3ZfkwO7a2bt-jZ-/exec',
 
   // Optional auth token or API key
   authToken: '',
